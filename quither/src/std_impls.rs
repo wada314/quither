ef91a57ff0b754b6671d99c57771f5d3b99650d4
Shohei Wada <pc@wada314.jp>
2025-05-03 20:39:27 +0900

    yarune

--- quither/src/std_impls.rs
+++ quither/src/std_impls.rs
@@ -23,14 +23,14 @@ use ::std::io::{BufRead, Read, Result as IoResult};
 
 /// Implement `Read` for `Quither<L, R>` if `L` and `R` implement `Read`.
 ///
-/// The implementation for `Left`, `Right` or `Neither` case are very trivial;
+/// The implementation for `Left`, `Right` or `Neither` case are trivial;
 /// it just returns the inner value's result directly, or returns `Ok(0)` if the variant is `Neither`.
 ///
 /// The implementation for `Both` case is little more complex, and is slightly different with
-/// the `Read::chain` method. For every call to `read`, it first tries to read data from `Left`,
-/// then from `Right` if there is no data read from `Left`.
-/// (where the `Read::chain` method will never try again to read from the first reader once it
-/// returns `Ok(0)`).
+/// the [`std::io::Read::chain`] method. For every call to `read`, it first tries to read data
+/// from `Left`, then from `Right` if there is no data read from `Left`.
+/// (where the [`std::io::Read::chain`] method will never try again to read from the first
+/// reader once it returns `Ok(0)`).
 #[cfg(feature = "use_std")]
 #[quither]
 impl<L, R> Read for Quither<L, R>
@@ -58,6 +58,43 @@ where
                     Ok(left_len)
                 }
             }
+        }
+    }
+}
+
+/// Implement `BufRead` for `Quither<L, R>` if `L` and `R` implement `BufRead`.
+///
+/// Surprisingly, this trait impl does not support a type containing `Both` variant,
+/// where the plain [`std::io::Read`] trait does.
+/// This is because of the `BufRead::consume` method cannot tell which reader to consume from.
+/// Instead, you can use [`std::io::Read::chain`] method to chain the readers together,
+/// so that you can combine two readers into one.
+#[cfg(feature = "use_std")]
+#[quither(!has_both)]
+impl<L, R> BufRead for Quither<L, R>
+where
+    L: BufRead,
+    R: BufRead,
+{
+    fn fill_buf(&mut self) -> IoResult<&[u8]> {
+        match self {
+            #[either]
+            Self::Left(l) => l.fill_buf(),
+            #[either]
+            Self::Right(r) => r.fill_buf(),
+            #[neither]
+            Self::Neither => Ok(&[]),
+        }
+    }
+
+    fn consume(&mut self, #[allow(unused)] amt: usize) {
+        match self {
+            #[either]
+            Self::Left(l) => l.consume(amt),
+            #[either]
+            Self::Right(r) => r.consume(amt),
+            #[neither]
+            Self::Neither => {}
         }
     }
 }
