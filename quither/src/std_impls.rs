e707fad7378e41938700a907e5aa68c312b828df
Shohei Wada <pc@wada314.jp>
2025-05-05 17:14:55 +0900

    boo

--- quither/src/std_impls.rs
+++ quither/src/std_impls.rs
@@ -231,3 +231,55 @@ where
         }
     }
 }
+
+impl<L, R> From<Result<R, L>> for Either<L, R> {
+    fn from(result: Result<R, L>) -> Self {
+        match result {
+            Ok(r) => Either::Right(r),
+            Err(l) => Either::Left(l),
+        }
+    }
+}
+
+/// Promotes a type without `Both` variant to a type with `Both` variant.
+#[quither(has_both && (has_either || has_neither))]
+impl<L, R> From<Quither<L, R, has_either, has_neither, false>> for Quither<L, R> {
+    fn from(quither: Quither<L, R, has_either, has_neither, false>) -> Self {
+        match quither {
+            #[either]
+            Quither::<L, R, has_either, has_neither, false>::Left(l) => Quither::Left(l),
+            #[either]
+            Quither::<L, R, has_either, has_neither, false>::Right(r) => Quither::Right(r),
+            #[neither]
+            Quither::<L, R, has_either, has_neither, false>::Neither => Quither::Neither,
+        }
+    }
+}
+
+/// Promotes a type without `Either` variant to a type with `Either` variant.
+#[quither(has_either && (has_neither || has_both))]
+impl<L, R> From<Quither<L, R, false, has_neither, has_both>> for Quither<L, R> {
+    fn from(quither: Quither<L, R, false, has_neither, has_both>) -> Self {
+        match quither {
+            #[neither]
+            Quither::<L, R, false, has_neither, has_both>::Neither => Quither::Neither,
+            #[both]
+            Quither::<L, R, false, has_neither, has_both>::Both(l, r) => Quither::Both(l, r),
+        }
+    }
+}
+
+/// Promotes a type without `Neither` variant to a type with `Neither` variant.
+#[quither(has_neither && (has_either || has_both))]
+impl<L, R> From<Quither<L, R, has_either, false, has_both>> for Quither<L, R> {
+    fn from(quither: Quither<L, R, has_either, false, has_both>) -> Self {
+        match quither {
+            #[either]
+            Quither::<L, R, has_either, false, has_both>::Left(l) => Quither::Left(l),
+            #[either]
+            Quither::<L, R, has_either, false, has_both>::Right(r) => Quither::Right(r),
+            #[both]
+            Quither::<L, R, has_either, false, has_both>::Both(l, r) => Quither::Both(l, r),
+        }
+    }
+}
